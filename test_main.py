7e2e9a14787f2e549d1ce70ee7fbbe813232a948
Zhengming Ding <allanzmding@gmail.com>
2024-02-06 09:22:13 -0600

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,4 +4,4 @@ from main import *
 
 ## Feel free to add your own tests here.
 def test_multiply():
-    assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert quadratic_multiply(BinaryNumber(3), BinaryNumber(3)) == 3*3
